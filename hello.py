b13a4ab1199b637e901efc7bb4c205d8bd0c2c92
raffathsaroos <raffaq8sarose@gmail.com>
2026-09-04 11:28:39 +0530

    first commit

---
+++ hello.py
@@ -0,0 +1,14 @@
+print("Hello, Job Tracker!")
+
+company_name = "Google"
+job_title = "Backend Developer"
+salary = 120000
+is_remote = True
+
+print(f"Company: {company_name}")
+print(f"Role: {job_title}")
+print(f"Salary: {salary}")
+print(f"Remote or not: {is_remote}")
+
+user_name = input("Enter Name: ")
+print(f"Welcom to Google: {user_name}!")
